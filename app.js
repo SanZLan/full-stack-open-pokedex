37e5cc4ec058d2d3f711cd35a896f05a661d7c97
Santiago Zuluaga <szuluagalangton@hotmail.com>
2024-08-29 22:51:33 -0500

    Completed exercise 11.11

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express')
+import express, { static as serveStatic } from 'express'
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(serveStatic('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
+})
+
+app.get('/version', (req, res) => {
+  res.send('Pokedex version: 2')
 })
